3ba56d556ac53494d8216cdf40056b8d97e41bf5
LucasX <xulu0620@gmail.com>
2019-05-08 00:00:40 +0800

    update DOC

--- viz/visualize.py
+++ viz/visualize.py
@@ -1,63 +1,17 @@
 import sys
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 
 import cv2
-from PIL import Image
-from sklearn.manifold import TSNE
-from skimage import io
-from skimage.transform import resize
-
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
+from PIL import Image
+from skimage import io
 from torchvision.transforms import transforms
 
 sys.path.append('../')
 from models.hmtnet_fbp import HMTNet
-
-
-def viz_race(wj=True, hmt_model='./model/hmt-net.pth'):
-    """
-    UNFINISHED YET!!
-    visualize race representation in high dimension
-    :param wj: with joint-training or not
-    :return:
-    """
-    hmt_net = HMTNet()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    if torch.cuda.device_count() > 1:
-        print("We are running on", torch.cuda.device_count(), "GPUs!")
-        hmt_net = nn.DataParallel(hmt_net)
-
-    hmt_net.load_state_dict(torch.load(hmt_model))
-    hmt_net.to(device)
-    hmt_net.eval()
-
-    X = np.zeros([4, 4])  # just for test
-    tsne = TSNE(n_components=2)
-    tsne.fit_transform(X)
-
-    img_path = "E:/DataSet/Face/SCUT-FBP5500/Images/ftw8.jpg"
-    image = resize(io.imread(img_path), (224, 224), mode='constant')
-    image[:, :, 0] -= 131.45376586914062
-    image[:, :, 1] -= 103.98748016357422
-    image[:, :, 2] -= 91.46234893798828
-
-    image = np.transpose(image, [2, 0, 1])
-    input = torch.from_numpy(image).unsqueeze(0).float()
-    input = input.to(device)
-
-    for idx, module in hmt_net.named_children():
-        if idx != 'conv4':
-            input = module(input)
-        else:
-            for id, mod in module.named_children():
-                if id == 'rrelu2':
-                    # get the feature representation
-                    print(input.shape)
-                else:
-                    input = mod(input)
 
 
 def heatmap(data, ax=None,
@@ -218,9 +172,9 @@ def viz_feature_map(image_file, hmtnet_model_file='../main/model/hmt-net-fbp.pth
     mat = np.average(mat, axis=2)
     # mat = 0.9 * mat + 0.1 * np.average(np.transpose(image, [1, 2, 0]), axis=2)
 
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(mat)
-    # plt.show()
+    fig, ax = plt.subplots()
+    im = ax.imshow(mat)
+    plt.show()
 
     # newly added
     heatmap = np.maximum(mat, 0)
