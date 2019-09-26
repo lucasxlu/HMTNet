7b9bbc125a47543c0a8f34eeeea850e605f7fb42
LucasX <xulu0620@gmail.com>
2019-09-26 10:44:06 +0800

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -12,16 +12,16 @@ from config.cfg import cfg
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.Resize(227),
-        transforms.RandomRotation(30),
+        transforms.Resize(256),
         transforms.RandomCrop(224),
-        transforms.ToTensor(),
+        transforms.RandomRotation(30),
         transforms.ColorJitter(),
+        transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'test': transforms.Compose([
-        transforms.Resize(227),
-        transforms.RandomCrop(224),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
